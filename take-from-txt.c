c6566fe6389f973ed1a1cf53794bba3b2bad2bf0
Maelwenn <maelwenn@labidurie.fr>
2022-11-09 15:34:22 +0100

    implement take-from-txt.h

---
+++ take-from-txt.c
@@ -0,0 +1,16 @@
+#include "take-from-txt.h"
+
+
+char* extractAgreeFormeFromLine(char* line){
+    return "stabilisaient";
+}
+
+
+char* extractBaseFormeFromLine(char* line){
+    return "stabiliser";
+
+}
+
+char* extractInformationFromLine(char* line){
+    return "Nom:Mas+SG";
+}
